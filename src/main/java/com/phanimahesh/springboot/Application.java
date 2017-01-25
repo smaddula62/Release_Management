abbc6aa4197964d858ab1692d33b2f43d2776637
smaddula62 <smaddula@miraclesoft.com>
2017-01-25 00:54:34 -0500

    payme

---
+++ src/main/java/com/phanimahesh/springboot/Application.java
@@ -0,0 +1,19 @@
+package com.phanimahesh.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Created by Amma on 1/17/2017.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+    }
+
+}
